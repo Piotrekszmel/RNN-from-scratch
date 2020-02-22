083fe34b222ae10df3c1be4648e46b069c957684
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-02-22 13:01:05 +0100

    create Softmax class

--- utils.py
+++ utils.py
@@ -7,3 +7,17 @@ class Sigmoid:
     
     def backward(self, x):
         return (1.0 - x) * x
+
+
+class Softmax:
+    def forward(self, x):
+        exp_x = np.exp(-x)
+        return exp_x / np.sum(exp_x)
+    
+    def loss(self, x, y):
+        return np.log[x[y]]
+    
+    def backward(self, x, y):
+        x[y] -= 1.0
+        return x
+    
